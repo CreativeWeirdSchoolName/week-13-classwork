d35bfebfbc02aa34ebeda79c2215a9a7a6220b40
Noel Smith <noelsmith@Noels-MacBook-Pro-2.local>
2025-04-24 09:21:31 -0400

    Level 2 Complete

---
+++ level_2.py
@@ -0,0 +1,6 @@
+mix_of_stuff = ["one integer", "one string", "one float", "one boolean"]
+
+playlist_for_coding = ["Escape From Midwich Valley", "Reload (Wangline's \"MEGATON MISSLE\" Remix)", "Straight Outta Hell", "Swvrm", "Literally just the whole Matrix soundtrack"]
+
+playlist_for_coding.append ("Never Gonna Give You Up by Rick Astley")
+
