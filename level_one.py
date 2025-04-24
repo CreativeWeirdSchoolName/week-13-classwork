e18c989bc84fe914029c67350955f254e73f502b
Noel Smith <noelsmith@Noels-MacBook-Pro-2.local>
2025-04-24 08:59:41 -0400

    Level One Complete For Real This Time, Sorry for got to save before pushing

---
+++ level_one.py
@@ -0,0 +1,9 @@
+#Level One
+#print("\"What do you call a fish with no eyes?\" I paused, puzzled, until he grinned at me and said, \"Fsh!\"")
+
+#print("\"What do birdshand out on Halloween?\", she said. \"Tweets\"")
+
+#print("Up \nDown")
+
+print("Line 1 \n Line 2 \n  Line 3 \n  Line 4 \nLine5")
+
